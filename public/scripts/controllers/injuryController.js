b73cb183b481325cc8db26d3e5134d26d7a536b1
Moises Miguel <moisesmigueledu@gmail.com>
2017-07-21 09:56:09 -0500

    finsished checking for errors

--- public/scripts/controllers/injuryController.js
+++ public/scripts/controllers/injuryController.js
@@ -1,7 +1,36 @@
 function injuryController() {
     var vm = this;
-
+    var count = 0;
     vm.submit = function() {
         console.log('clicked');
+
+        injuryObject = {
+            "SCI Relation": vm.sciRelation,
+            cause: vm.cause,
+            "Year of Injury":vm.yearInjury,
+            level: vm.level,
+            "ASIA Score": vm.asia,
+
+        };
+
+        count = checkOBject(injuryObject);
+
+        if (count ===0) {
+            console.log('object is ready', injuryObject);
+        }
     };//end of submit
+}
+
+function checkOBject(object) {
+    var count =0;
+    for(var x in object) {
+        if (object.hasOwnProperty(x)) {
+            if (object[x] === undefined || object[x] === null || object[x] === "") {
+                alert('Please fill out ' + x);
+                count++;
+                break;
+            }
+        }
+    }
+    return count;
 }
