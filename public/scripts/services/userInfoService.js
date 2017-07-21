bb3f3b38b18400771d8030bd2e78965aca88857f
Brett Miller <miller.brett.andrew@gmail.com>
2017-07-21 16:32:34 -0500

    added questions to question page

--- public/scripts/services/userInfoService.js
+++ public/scripts/services/userInfoService.js
@@ -1,18 +1,18 @@
 myApp.service('UserInfoService', function($http) {
-    var sv = this;
-    console.log('inside of ');
-    //will contain the users full information
-    var fulluserObject = {};
+  var sv = this;
+  console.log('inside of ');
+  //will contain the users full information
+  var fulluserObject = {};
 
-    sv.getUserInfo = function(data) {
-        console.log('inside of getUserInfo');
-        if (data.id === "injury") {
-            fulluserObject.injury = data;
-        } else if(data.id === "signup") {
-            fulluserObject.signup = data;
-        }
-        localStorage.setItem("fulluserObject", fulluserObject );
-        console.log(localStorage.getItem("fulluserObject"));
-    };//end of getUserInfo
+  sv.getUserInfo = function(data) {
+    console.log('inside of getUserInfo');
+    if (data.id === "injury") {
+      fulluserObject.injury = data;
+    } else if (data.id === "signup") {
+      fulluserObject.signup = data;
+    }
+    localStorage.setItem("fulluserObject", fulluserObject);
+    console.log(localStorage.getItem("fulluserObject"));
+  }; //end of getUserInfo
 
 });
