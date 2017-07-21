83411d55f7c57ec1d11e630b3748ac8203a94498
Brett Miller <miller.brett.andrew@gmail.com>
2017-07-21 14:51:48 -0500

    injury object to send

--- public/scripts/controllers/injuryController.js
+++ public/scripts/controllers/injuryController.js
@@ -1,36 +1,51 @@
 function injuryController() {
-    var vm = this;
-    var count = 0;
-    vm.submit = function() {
-        console.log('clicked');
+  var vm = this;
+  var count = 0;
+  vm.levelArray = [];
 
-        injuryObject = {
-            "SCI Relation": vm.sciRelation,
-            cause: vm.cause,
-            "Year of Injury":vm.yearInjury,
-            level: vm.level,
-            "ASIA Score": vm.asia,
+  vm.submit = function() {
+    console.log('clicked');
 
-        };
+    var l = document.getElementsByName('level');
 
-        count = checkOBject(injuryObject);
+    for (var i = 0; i < l.length; i++) {
+      if (l[i].checked) {
+        console.log('this is true');
+        vm.levelType = l[i].defaultValue;
 
-        if (count ===0) {
-            console.log('object is ready', injuryObject);
-        }
-    };//end of submit
+        console.log(vm.levelType);
+        vm.levelArray.push(vm.levelType);
+      }
+      console.log(l[i].checked);
+    } //end of loop
+
+    injuryObject = {
+      id: 'injury',
+      "SCI Relation": vm.sciRelation,
+      cause: vm.cause,
+      "Year of Injury": vm.yearInjury,
+      level: vm.levelArray,
+      "ASIA Score": vm.asia,
+    };
+
+    count = checkOBject(injuryObject);
+
+    if (count === 0) {
+      console.log('object is ready', injuryObject);
+    }
+  }; //end of submit
 }
 
 function checkOBject(object) {
-    var count =0;
-    for(var x in object) {
-        if (object.hasOwnProperty(x)) {
-            if (object[x] === undefined || object[x] === null || object[x] === "") {
-                alert('Please fill out ' + x);
-                count++;
-                break;
-            }
-        }
+  var count = 0;
+  for (var x in object) {
+    if (object.hasOwnProperty(x)) {
+      if (object[x] === undefined || object[x] === null || object[x] === "") {
+        alert('Please fill out ' + x);
+        count++;
+        break;
+      }
     }
-    return count;
+  }
+  return count;
 }
