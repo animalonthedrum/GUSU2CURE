68a099b2b8964d82df607ee5cef73a5262a22e37
Moises Miguel <moisesmigueledu@gmail.com>
2017-07-24 15:18:26 -0500

    added dummy object

--- public/scripts/services/userInfoService.js
+++ public/scripts/services/userInfoService.js
@@ -1,8 +1,62 @@
-myApp.service('UserInfoService', function($http) {
+	myApp.service('UserInfoService', function($http) {
 
 	var sv = this;
 
 	//will contain the users full information
+	sv.dummyObject = {};
+	sv.dummyObject.bio = {
+		edLevel:"Some College",
+		famStatus:"childrenAtHome",
+		id:"bio",
+		lang:"2",
+		relStatus:"married"
+	}
+	sv.dummyObject.hobbies = {
+		dateStamp:"7/24/2017",
+		hobbies:['basketball', 'crafting', 'baking'],
+		id:'hobbies'
+	}
+	sv.dummyObject.hobbies = {
+		asia:"B",
+		cause:"2",
+		id:"injury",
+		level:['C1-C4', 'C5'],
+		sciRel:"Parent",
+		yrInjury:1999
+	}
+	sv.dummyObject.login = {
+		confirmPassword:"go",
+		email:"moises@gmail.com",
+		firstName:"moises",
+		id:"login",
+		lastName:"miguel",
+		password:"go",
+		terms:true
+	}
+
+	sv.dummyObject.question = {
+		additional:"asdfasd",
+		comment:"asdfasdf",
+		experience:"adsfasdf",
+		heardAbout:['Personal Reference'],
+		id:"question",
+		question:"Yes",
+	}
+	sv.dummyObject.signup = {
+		address:"asdfasdf",
+		city:"asdfasdf",
+		dob:'Thu Aug 03 2017 00:00:00 GMT-0500 (CDT)',
+		gender:"Male",
+		id:"signup",
+		phone:"9878764563",
+		phoneType:"Home",
+		state:"mn",
+		typeOfUser:"mentor",
+		visitType:['Hospital'],
+		zip:55044
+	}
+
+
 	sv.fulluserObject = {};
 
 	sv.getUserInfo = function(data) {
