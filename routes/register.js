26a73540b05c9bc9907a41928ae3dcd00bfb8747
BluePawDev <jaruby@me.com>
2017-07-25 14:20:53 -0500

    Add full INSERT query back to registration, minus dependant table data

--- routes/register.js
+++ routes/register.js
@@ -46,7 +46,7 @@ router.post('/', function(req, res) {
 							var email = req.body.login.email;
 							var dateCreated = req.body.hobbies.dateStamp;
 							var accessLvl = req.body.signup.typeOfUser;
-							// var enabled = TRUE;
+							var enabled = true;
 							var first = req.body.login.firstName;
 							var last = req.body.login.lastName;
 							var dob = req.body.signup.dob;
@@ -59,28 +59,29 @@ router.post('/', function(req, res) {
 							var zip = req.body.signup.zip;
 							// var visit_pref;
 							// var sci_rel;
-							var sci_cause = req.body.signup.cause;
-							// var sci_age;
+							// var sci_cause = req.body.signup.cause;
+							var sci_age = req.body.injury.injAge;
 							var sci_lvl = req.body.injury.level;
 							var asia = req.body.injury.asia;
 							var mobility = req.body.injury.mobility;
 							// var trans_type;
-							var rel_status = req.body.bio.relStatus;
+							// var rel_status = req.body.bio.relStatus;
 							var fam_status = req.body.bio.famStatus;
 							var ed_lvl = req.body.bio.edLevel;
 							// var emp;
-							var lang = req.body.bio.lang;
+							// var lang = req.body.bio.lang;
 							var pets = req.body.bio.pets;
-							var hobbies;
+							// var hobbies;
+							var questions = req.body.question.question;
+							var experience = req.body.question.experience;
+							var additional = req.body.question.additional;
+							var comments = req.body.question.comment;
+							var heardAbout = req.body.question.heardAbout;
 							var password = hash;
 							// END query variables
 
 							// START INSERT query
-							connection.query("INSERT INTO tbl_user (email, first_name, last_name, password) VALUES ('" + email + "','" + first + "','" + last + "','" + password + "');")
-
-
-
-							// connection.query("INSERT INTO tbl_user (email, first_name, last_name, dob, gender, phone, phone_type, street, city, state, zip, asia_score, fam_status, sci_lvl, mobility_req, ed_lvl, pets, date_created) VALUES ('" + email + "', '" + first + "', '" + last + "', '" + dob + "', '" + gender + "', '" + phone + "', '" + phoneType + "', '" + street + "', '" + city + "', '" + state + "', '" + zip + "','" + asia + "', '" + fam_status + "','" + sci_lvl + "','" + mobility + "','" + ed_lvl + "', '" + pets + "', '" + dateCreated + "');")
+							connection.query("INSERT INTO tbl_user (email, date_created, access_lvl, enabled, first_name, last_name, password, dob, gender, phone, phone_type, street, city, state, zip, sci_age, sci_lvl, asia_score, mobility_req, fam_status, ed_lvl, pets, questions, experience, additional, comments) VALUES ('" + email + "','" + dateCreated + "','" + accessLvl + "','" + enabled + "','" + first + "','" + last + "','" + password + "','" + dob + "','" + gender + "','" + phone + "','" + phoneType + "','" + street + "','" + city + "','" + state + "','" + zip + "','" + sci_age + "','" + sci_lvl + "','" + asia + "','" + mobility + "','" + fam_status + "','" + ed_lvl + "', '" + pets + "','" + questions + "','" + experience + "','" + additional + "','" + comments + "');")
 							// END INSERT query
 
 							done();
