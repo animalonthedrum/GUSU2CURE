6ee2d346a35a3f7016ac228c343857ac1377716e
BluePawDev <jaruby@me.com>
2017-07-25 16:06:47 -0500

    Add userInfo.js file and router to server.js

--- public/scripts/services/userPageService.js
+++ public/scripts/services/userPageService.js
@@ -1,21 +1,63 @@
 myApp.service('userPageService', function($http) {
-    var sv = this;
+	var sv = this;
 
-    sv.saveUserInfo = function(user) {
-        if (user === undefined) {
-            sv.userInfo = JSON.parse(localStorage.getItem('userData'));
-            console.log('sv.userInfo', sv.userInfo);
-            var email = sv.userInfo.email;
-            console.log(email);
-            return $http.post('/login', email ).then(function(res) {
-                console.log('back from the server with', res);
-            });
+	sv.saveUserInfo = function(user) {
+		if (user === undefined) {
+			sv.userInfo = JSON.parse(localStorage.getItem('userData'));
+			console.log('sv.userInfo', sv.userInfo);
+			var email = sv.userInfo.email;
+			console.log(email);
+			return $http.post('/login', email).then(function(res) {
+				console.log('back from the server with', res);
+			});
+		} else {
+			var userLogin = user[0];
+			localStorage.setItem('userData', JSON.stringify(userLogin));
+			sv.userdata = user;
+		}
+	}; //end of saveUserInfo
 
-        } else {
-            var userLogin = user[0];
-            localStorage.setItem('userData', JSON.stringify(userLogin));
-            sv.userdata = user;
 
-        }
-    };//end of saveUserInfo
-});//end of service
+	// START getUserInfo
+	sv.getUserInfo = function(user) {
+		if (user === undefined) {
+			sv.userInfo = JSON.parse(localStorage.getItem('userData'));
+			console.log('sv.userInfo', sv.userInfo);
+			var email = sv.userInfo.email;
+			console.log(email);
+			return $http.get('/userInfo', email).then(function(res) {
+				console.log('Back from userInfo.js with:', res);
+			});
+		} else {
+			var userLogin = user[0];
+			localStorage.setItem('userData', JSON.stringify(userLogin));
+			sv.userdata = user;
+		}
+	}; //end of getUserInfo
+
+	// START updateUserInfo
+	sv.updateUserInfo = function(user) {
+		if (user === undefined) {
+			sv.userInfo = JSON.parse(localStorage.getItem('userData'));
+			console.log('sv.userInfo', sv.userInfo);
+			var email = sv.userInfo.email;
+			console.log(email);
+			return $http.put('/userInfo', email).then(function(res) {
+				console.log('Back from userInfo.js with:', res);
+			});
+		} else {
+			var userLogin = user[0];
+			localStorage.setItem('userData', JSON.stringify(userLogin));
+			sv.userdata = user;
+		}
+	}; //end of updateUserInfo
+
+
+
+
+
+
+
+
+
+}); //end of service
---
+++ routes/userInfo.js
@@ -0,0 +1,99 @@
+/* REQUIRES for userInfo.js */
+var express = require('express');
+var router = express.Router();
+var pg = require('pg');
+var bodyParser = require('body-parser');
+var bcrypt = require('bcrypt');
+
+/* USES for userInfo.js */
+router.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+router.use(bodyParser.json());
+
+/* CONFIG POOL */
+var config = {
+	database: 'db_gusu',
+	host: 'localhost',
+	port: '5432',
+	max: 20
+};
+
+var pool = new pg.Pool(config);
+
+
+// START GET userInfo
+router.post('/', function(req, res) {
+	// START dB connection
+	pool.connect(function(err, connection, done) {
+		if (err) {
+			done();
+			res.send('pool connect err:', err);
+		} else {
+			// START salt generation
+			bcrypt.genSalt(12, function(err, salt) {
+				if (err) {
+					console.log('getSalt err:', err);
+				} else {
+					// START hash generation
+					bcrypt.hash(req.body.login.password, salt, function(err, hash) {
+						// START INSERT new user query
+						if (err) {
+							res.send('hash gen err:', err);
+						} else {
+							// START query variables
+							var email = req.body.login.email;
+							var dateCreated = req.body.hobbies.dateStamp;
+							var accessLvl = req.body.signup.typeOfUser;
+							var enabled = true;
+							var first = req.body.login.firstName;
+							var last = req.body.login.lastName;
+							var dob = req.body.signup.dob;
+							var gender = req.body.signup.gender;
+							var phone = req.body.signup.phone;
+							var phoneType = req.body.signup.phoneType;
+							var street = req.body.signup.address;
+							var city = req.body.signup.city;
+							var state = req.body.signup.state;
+							var zip = req.body.signup.zip;
+							// var visit_pref;
+							// var sci_rel;
+							// var sci_cause = req.body.signup.cause;
+							var sci_age = req.body.injury.injAge;
+							var sci_lvl = req.body.injury.level;
+							var asia = req.body.injury.asia;
+							var mobility = req.body.injury.mobility;
+							// var trans_type;
+							// var rel_status = req.body.bio.relStatus;
+							var fam_status = req.body.bio.famStatus;
+							var ed_lvl = req.body.bio.edLevel;
+							// var emp;
+							// var lang = req.body.bio.lang;
+							var pets = req.body.bio.pets;
+							// var hobbies;
+							var questions = req.body.question.question;
+							var experience = req.body.question.experience;
+							var additional = req.body.question.additional;
+							var comments = req.body.question.comment;
+							var heardAbout = req.body.question.heardAbout;
+							var password = hash;
+							// END query variables
+
+							// START INSERT query
+							connection.query("INSERT INTO tbl_user (email, date_created, access_lvl, enabled, first_name, last_name, password, dob, gender, phone, phone_type, street, city, state, zip, sci_age, sci_lvl, asia_score, mobility_req, fam_status, ed_lvl, pets, questions, experience, additional, comments) VALUES ('" + email + "','" + dateCreated + "','" + accessLvl + "','" + enabled + "','" + first + "','" + last + "','" + password + "','" + dob + "','" + gender + "','" + phone + "','" + phoneType + "','" + street + "','" + city + "','" + state + "','" + zip + "','" + sci_age + "','" + sci_lvl + "','" + asia + "','" + mobility + "','" + fam_status + "','" + ed_lvl + "', '" + pets + "','" + questions + "','" + experience + "','" + additional + "','" + comments + "');")
+							// END INSERT query
+
+							done();
+							res.send('user created');
+						}
+					}) // END INSERT user user query
+				}
+			}) // END salt generation
+		}
+	}) // END dB connection
+})
+// END GET userInfo
+
+/* EXPORTS for userInfo.js */
+module.exports = router;
