5ad22c56996cbb093c66f04f3544d09017747586
Brett Miller <miller.brett.andrew@gmail.com>
2017-07-25 09:42:28 -0500

    progess bar and views in order

--- public/scripts/controllers/hobbyController.js
+++ public/scripts/controllers/hobbyController.js
@@ -3,55 +3,55 @@ function hobbyController(UserInfoService) {
   var count = 0;
 
   vm.submit = function() {
-      console.log('clicked ' );
-      var hobbies = document.getElementsByName('hobby');
-      vm.hobbiesArr = [];
+    console.log('clicked ');
+    var hobbies = document.getElementsByName('hobby');
+    vm.hobbiesArr = [];
 
-      //creates a date stamp
-      var month = new Date().getMonth() + 1;
-      var year = new Date().getFullYear();
-      var day = new Date().getDate();
-      var dateUserCreated = month + "/" + day + "/" + year;
+    //creates a date stamp
+    var month = new Date().getMonth() + 1;
+    var year = new Date().getFullYear();
+    var day = new Date().getDate();
+    var dateUserCreated = month + "/" + day + "/" + year;
 
 
-      //adds hobbies to vm.hobbiesArr
-      for (var i = 0; i < hobbies.length; i++) {
-         if (hobbies[i].checked) {
-             vm.hobbiesArr.push(hobbies[i].defaultValue);
-         }
-     }//end of for loop
+    //adds hobbies to vm.hobbiesArr
+    for (var i = 0; i < hobbies.length; i++) {
+      if (hobbies[i].checked) {
+        vm.hobbiesArr.push(hobbies[i].defaultValue);
+      }
+    } //end of for loop
 
-      var hobbiesObject = {
-          id:'hobbies',
-          hobbies: vm.hobbiesArr,
-          dateStamp:dateUserCreated
-      };//end of hobbiesArr
+    var hobbiesObject = {
+      id: 'hobbies',
+      hobbies: vm.hobbiesArr,
+      dateStamp: dateUserCreated
+    }; //end of hobbiesArr
 
-      count = checkObjectArr(hobbiesObject);
+    count = checkObjectArr(hobbiesObject);
 
-      //only runs if array has 3 or more items
-      if (count === 0) {
-          UserInfoService.getUserInfo(hobbiesObject);
-          UserInfoService.sendRegistration();
-      }//end of conditional statement
-  };//end of submit
-}//end of controller
+    //only runs if array has 3 or more items
+    if (count === 0) {
+      UserInfoService.getUserInfo(hobbiesObject);
+      UserInfoService.sendRegistration();
+    } //end of conditional statement
+  }; //end of submit
+} //end of controller
 
 //checks if array is valid
 function checkObjectArr(object) {
-    var count = 0;
-    for(var x in object ) {
-        if (object.hasOwnProperty(x)) {
-            if (object[x].length < 3) {
-                sweetAlert({
-                           title: "Hobbies",
-                           text: "Please choose atleast three hobies",
-                           type: "warning"
-                       });//end of sweetAlert
-                count++;
-                break;
-            }
-        }
+  var count = 0;
+  for (var x in object) {
+    if (object.hasOwnProperty(x)) {
+      if (object[x].length < 3) {
+        sweetAlert({
+          title: "Hobbies",
+          text: "Please choose atleast three hobbies",
+          type: "warning"
+        }); //end of sweetAlert
+        count++;
+        break;
+      }
     }
-    return count;
+  }
+  return count;
 }
