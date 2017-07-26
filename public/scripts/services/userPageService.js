9b99f79ccc2f7e47b4b0d581665f281bce201953
BluePawDev <jaruby@me.com>
2017-07-26 11:34:28 -0500

    Add POST to userInfo.js to get user data

--- public/scripts/services/userPageService.js
+++ public/scripts/services/userPageService.js
@@ -18,24 +18,17 @@ myApp.service('userPageService', function($http) {
 
 
 	// START getUserInfo
-	sv.getUserInfo = function(user) {
+	sv.getUserInfo = function() {
 		sv.user = JSON.parse(localStorage.getItem('userData'));
 		sv.email = sv.user.email;
-		console.log(sv.email);
-		if (user === undefined) {
-			// sv.userInfo = JSON.parse(localStorage.getItem('userData'));
-			console.log('sv.userInfo', sv.userInfo);
-			return $http.post('/userInfo', sv.email).then(function(res) {
-				console.log('Back from userInfo.js with:', res);
-			});
-		} else {
-			var userLogin = user[0];
-			localStorage.setItem('userData', JSON.stringify(userLogin));
-			// return $http.post('/userInfo', sv.email).then(function(res) {
-			// console.log('Back from userInfo.js get with:', res);
-			// })
-			// sv.userdata = user;
+		var sendEmail = {
+			email: sv.email
 		}
+		// console.log(sendEmail);
+		// console.log(sv.email);
+		return $http.post('/userInfo', sendEmail).then(function(res) {
+			console.log('Back from userInfo.js with:', res);
+		});
 	}; // END getUserInfo
 
 	// START updateUserInfo
