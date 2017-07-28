84ffc8a0b218655b3bbe30bcb218b61042696a5e
Moises Miguel <moisesmigueledu@gmail.com>
2017-07-28 11:52:38 -0500

    added search

--- public/scripts/controllers/adminController.js
+++ public/scripts/controllers/adminController.js
@@ -1,7 +1,9 @@
 function adminController(adminService, UserInfoService) {
   var vm = this;
+  vm.users = [];
   vm.mentorsMentees = function() {
       adminService.getMentorsMentees().then(function() {
+
           vm.users = adminService.users;
           vm.users.forEach(function(user) {
               if (user.access_lvl === 1) {
@@ -16,7 +18,7 @@ function adminController(adminService, UserInfoService) {
               vm.enabled = vm.users[i].enabled;
           }
       });//end of promise
-
+      return vm.users;
   };//end of function
 
   vm.switchEnabled = function(index) {
@@ -36,5 +38,33 @@ function adminController(adminService, UserInfoService) {
       UserInfoService.seeOtherUsersPage(vm.userToLogIn);
       window.location.href = '#!/visit';
   };//end of function
+
+
+  vm.search = function() {
+      console.log(vm.users);
+       adminService.getMentorsMentees().then(function() {
+           vm.typeUserSearch = adminService.users;
+
+           for (var i = 0; i < vm.typeUserSearch.length; i++) {
+               if (vm.searchUserBy === vm.typeUserSearch[i].first_name) {
+                   vm.users.pop([i]);
+                   vm.users.unshift(vm.typeUserSearch[i]);
+
+               }
+           }//end of for loop
+           vm.users.forEach(function(user) {
+               if (user.access_lvl === 1) {
+                   user.access_lvl = 'Mentor';
+                   return user.access_lvl;
+               } else if (user.access_lvl === 2) {
+                   user.access_lvl = 'Mentee';
+                   return user.access_lvl;
+               }//end of conditional statement
+           });//end of loop
+       });//end of promise
+
+  };//end of searchUser
+
+
 
 }//end of controller
