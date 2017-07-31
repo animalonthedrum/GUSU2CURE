733803e70d72180c8aa029a26d213b325cf6c707
Moises Miguel <moisesmigueledu@gmail.com>
2017-07-31 16:25:45 -0500

    added file share

--- public/scripts/controllers/userController.js
+++ public/scripts/controllers/userController.js
@@ -1,7 +1,30 @@
 function userController(UserInfoService, userPageService) {
   var vm = this;
+
+  //globals
+
+
   vm.getUserInfo = function() {
       console.log('in this');
-      userPageService.getUserInfo();
+      userPageService.getUserInfo().then(function() {
+          console.log('user info', userPageService.userLoggedInInfo);
+          vm.thisUser = userPageService.userLoggedInInfo;
+      });
+
   };//end of getUserInfo
+
+  vm.uploadImg = filestack.init('Ad5IIaaqyTY60IGIwPCg9z');
+  vm.showPicker = function() {
+    vm.uploadImg.pick({}).then(function(response) {
+      // console.log(response);
+
+      vm.img = response.filesUploaded[0].url;
+      userPageService.updateImage(vm.img).then(function() {
+          console.log(userPageService.userImage);
+          vm.userImage = userPageService.userImage;
+      });
+    }); //end uploadImg
+  };
+
+
 }//end of controller
