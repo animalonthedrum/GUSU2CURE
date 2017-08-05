d1106dc2587245675b6f88466875e0800ae6cd2d
Brett Miller <miller.brett.andrew@gmail.com>
2017-08-05 11:29:34 -0500

    user styles

--- public/scripts/controllers/userController.js
+++ public/scripts/controllers/userController.js
@@ -30,28 +30,29 @@ function userController(UserInfoService, userPageService, adminService) {
 
 
   vm.submitUserBio = function() {
-      userPageService.submitBio(vm.userBio);
-  };//end of submitUserBio
+    userPageService.submitBio(vm.userBio);
+  }; //end of submitUserBio
 
   vm.showMatches = function() {
-        var userToMatch = {
-          email: vm.thisUser.email,
-          type: 'Mentor',
-          asia: vm.thisUser.asia_score,
-          ed_lvl: vm.thisUser.ed_lvl,
-          gender: vm.thisUser.gender,
-          matched: vm.thisUser.matched,
-          pets: vm.thisUser.pets,
-          sci_age: vm.thisUser.sci_age,
-          rel_status: "Single",
-          sci_cause: "Fall",
-          zip: vm.thisUser.zip,
-          lang: "English",
-          fam_status: vm.thisUser.fam_status,
-          age: 59
-        };
-        adminService.matchingUsers(userToMatch).then(function() {
-          vm.matchContent = adminService.matchedUsers;
-        });
+    var userToMatch = {
+      email: vm.thisUser.email,
+      type: 'Mentor',
+      asia: vm.thisUser.asia_score,
+      ed_lvl: vm.thisUser.ed_lvl,
+      gender: vm.thisUser.gender,
+      matched: vm.thisUser.matched,
+      pets: vm.thisUser.pets,
+      sci_age: vm.thisUser.sci_age,
+      rel_status: "Single",
+      sci_cause: "Fall",
+      zip: vm.thisUser.zip,
+      lang: "English",
+      fam_status: vm.thisUser.fam_status,
+      age: 59
     };
+    adminService.matchingUsers(userToMatch).then(function() {
+      vm.matchContent = adminService.matchedUsers;
+    });
+  };
+
 } //end of controller
