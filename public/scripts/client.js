7c884e4956f8e2a055b53dc14d231c04101764b3
Moises Miguel <moisesmigueledu@gmail.com>
2017-08-07 13:48:54 -0500

    .

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -47,3 +47,29 @@ myApp.config(function($routeProvider) {
       controller: "visitUserPage as vc"
     });
 });
+
+
+
+myApp.filter('phoneNumber', function () {
+        return function (number) {
+            if (!number) { return ''; }
+            number = String(number);
+            number = number.replace(/[^0-9]*/g, '');
+            var formattedNumber = number;
+
+            var c = (number[0] == '1') ? '1' : '';
+            number = number[0] == '1' ? number.slice(1) : number;
+
+            var area = number.substring(0, 3);
+            var front = number.substring(3, 6);
+            var end = number.substring(6, 10);
+
+            if (front) {
+                formattedNumber = (c + "(" + area + ") " + front);
+            }
+            if (end) {
+                formattedNumber += ("-" + end);
+            }
+            return formattedNumber;
+        };
+    });
